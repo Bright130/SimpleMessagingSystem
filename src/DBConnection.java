050e58a8f00fb1f0935632c8da4695ebfaa43d57
Bright <Bright130@hotmail.com>
2017-11-11 16:39:03 +0700

    Completed managing account

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -17,9 +17,7 @@ public class DBConnection {
 
     /**  Tester  **/
     public static void main(String[] args) {
-        Account account =  DBConnection.getAccount("aaa@a.com");
-        System.out.println(account.getEmail());
-        System.out.println(account.getPassword());
+        boolean a =DBConnection.deleteAccount(new Account ("aaa@a.com","000","2013-01-22 12:12:12"));
 
     }
 
@@ -49,7 +47,7 @@ public class DBConnection {
 
             resultSet = statement.executeQuery( "SELECT * FROM Account WHERE username like '"+email+"' ;");
 
-            if ( resultSet!=null) {
+            if ( resultSet.next()) {
 
                 account =  new Account(resultSet.getString("username"),
                                    resultSet.getString("passwordUser"),
@@ -65,42 +63,120 @@ public class DBConnection {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 
         }
-
         return account;
     }
 
-    public static ArrayList<Account> getAccounts() {
+    public static boolean createAccount(Account account) {
 
         Connection connection = openDB();
         if(connection == null)
-            return null;
-        ArrayList<Account> accounts = new ArrayList<Account>();
+            return false;
         Statement statement = null;
-        ResultSet resultSet = null;
+
         try{
             statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO Account (username,passwordUser,lastRefresh) " +
+                                        "VALUES  ('"+account.getEmail()+"'" +
+                                                ",'"+account.getPassword()+"'" +
+                                                ",'"+account.getLastUpdate()+"');"
+                                    );
 
-            resultSet = statement.executeQuery( "SELECT * FROM Account  ;");
-
-            while ( resultSet.next() ) {
-
-                accounts.add(new Account(resultSet.getString("username"),
-                        resultSet.getString("passwordUser"),
-                        resultSet.getString("lastRefresh")
-                ));
-            }
-
-            resultSet.close();
             statement.close();
+            connection.commit();
             connection.close();
-            System.out.println("Records created successfully");
+            System.out.println("Insert successfully");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-
+            return false;
         }
-
-        return accounts;
+        return true;
     }
+
+    public static boolean updateAccount(Account account) {
+
+        Connection connection = openDB();
+        if(connection == null)
+            return false;
+        Statement statement = null;
+
+        try{
+            statement = connection.createStatement();
+            statement.executeUpdate("UPDATE Account set "+
+                                        "passwordUser = '"+account.getPassword()+"',"+
+                                        "lastRefresh = '"+account.getLastUpdate()+"' "+
+                                        "WHERE username like '"+account.getEmail()+"' "+
+                                        " ;"
+            );
+
+            statement.close();
+            connection.commit();
+            connection.close();
+            System.out.println("Update successfully");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteAccount(Account account) {
+
+        Connection connection = openDB();
+        if(connection == null)
+            return false;
+        Statement statement = null;
+
+        try{
+
+            statement = connection.createStatement();
+            statement.executeUpdate("DELETE from Account "+
+                    "WHERE username like '"+account.getEmail()+"' "+
+                    " ;"
+            );
+
+            statement.close();
+            connection.commit();
+            connection.close();
+            System.out.println("Delete successfully");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            return false;
+        }
+        return true;
+    }
+
+
+//    public static ArrayList<Account> getAccounts() {
+//
+//        Connection connection = openDB();
+//        if(connection == null)
+//            return null;
+//        ArrayList<Account> accounts = new ArrayList<Account>();
+//        Statement statement = null;
+//        ResultSet resultSet = null;
+//        try{
+//            statement = connection.createStatement();
+//
+//            resultSet = statement.executeQuery( "SELECT * FROM Account  ;");
+//
+//            while ( resultSet.next() ) {
+//
+//                accounts.add(new Account(resultSet.getString("username"),
+//                        resultSet.getString("passwordUser"),
+//                        resultSet.getString("lastRefresh")
+//                ));
+//            }
+//
+//            resultSet.close();
+//            statement.close();
+//            connection.close();
+//            System.out.println("Records created successfully");
+//        } catch ( Exception e ) {
+//            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+//
+//        }
+//        return accounts;
+//    }
 
 
 
